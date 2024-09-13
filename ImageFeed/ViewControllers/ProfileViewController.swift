c8ef34d2b7eeaad715fc5fa8755c87d1a92b0f29
Darya Drobysheva <dadrobysheva@edu.hse.ru>
2024-09-14 02:22:26 +0300

    Added ProfileViewController and app icon

---
+++ ImageFeed/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Дарья Дробышева on 13.09.2024.
+//
+
+import Foundation
+
+final class ProfileViewController: UIViewController {
+    
+}
