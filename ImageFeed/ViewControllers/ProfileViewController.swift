b93ed39f876cdbbda11e65c45727677ee514cfd4
Darya Drobysheva <dadrobysheva@edu.hse.ru>
2024-09-14 20:52:53 +0300

    Added tab bar navigation

--- ImageFeed/ViewControllers/ProfileViewController.swift
+++ ImageFeed/ViewControllers/ProfileViewController.swift
@@ -5,8 +5,17 @@
 //  Created by Дарья Дробышева on 13.09.2024.
 //
 
-import Foundation
+import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var profileDescriptionLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    override func viewDidLoad() {
+        exitButton.setTitle("", for: .normal)
+    }
 }
