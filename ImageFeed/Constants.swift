aa50caed42c23487750e1730cd5cfdeab9534109
daria <dadrobysheva@edu.hse.ru>
2025-01-16 23:43:02 +0300

    Added constants for Unsplash API

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Дарья Дробышева on 16.01.2025.
+//
+
+import UIKit
+
+enum Constants {
+    static let accessKey = "hoqRvUB9lpsXDoryE6JEUECulgM3k-hOFWTEP2yNSDY"
+    static let secretKey = "UoiWVBL7qWgV_RpaT1j08-4Tu1PdixMuU77-faCidjk"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let defaultBaseURL: URL = URL(fileURLWithPath: "https://api.unsplash.com/")
+    static let accessScope = "public+read_user+write_likes"
+}
