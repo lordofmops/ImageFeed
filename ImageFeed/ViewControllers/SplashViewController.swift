88f9dc61eb30b30d71b2cdc6256a9d7408b22378
daria <dadrobysheva@edu.hse.ru>
2025-02-18 18:55:56 +0300

    Added UI blocking loading animation

--- ImageFeed/ViewControllers/SplashViewController.swift
+++ ImageFeed/ViewControllers/SplashViewController.swift
@@ -96,12 +96,12 @@ extension SplashViewController: AuthViewControllerDelegate {
     }
     
     private func fetchOAuthToken(_ code: String) {
-        ProgressHUD.animate()
+        UIBlockingProgressHUD.show()
         
         oauth2Service.fetchOAuthToken(code: code) { [weak self] result in
             guard let self else { return }
             
-            ProgressHUD.dismiss()
+            UIBlockingProgressHUD.dismiss()
             
             switch result {
             case .success(let token):
